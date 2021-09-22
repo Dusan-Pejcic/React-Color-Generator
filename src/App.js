ac164a50273329d572b9e64027134f6f53f29cbe
dusan pejcic <dushan.pejcic@googlemail.com>
2021-09-22 23:33:46 +0200

    form added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+
+import Values from 'values.js'; // https://github.com/noeldelgado/values.js
 
 function App() {
+
+  const [color, setColor] = useState([]);
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    console.log('hello');
+  }
   return (
-    <h3>react color generator app</h3>
+    <>
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={ handleSubmit }>
+        <input type="text" value={color} onChange={(e) => setColor( e.target.value)} placeholder='#f15025'/>
+        <button className="btn">submit</button>
+      </form>
+    </section>
+    <section className="colors">
+      <h4>list goes here</h4>
+    </section>
+    </>
   );
 }
 
