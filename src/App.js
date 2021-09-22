ed5d94ce181ab6837521af9cf0aa45872dc586e0
dusan pejcic <dushan.pejcic@googlemail.com>
2021-09-23 00:07:03 +0200

    value.js added, handleSubmit function added

--- src/App.js
+++ src/App.js
@@ -10,16 +10,26 @@ function App() {
 
   const handleSubmit = (e)=> {
     e.preventDefault();
-    console.log('hello');
+
+    try{
+      let colors = new Values(color).all(10); // a values.js function to return tints and shades for a passed hexadecimal value.
+      setError(false);
+    }
+    catch(err){ // values.j throws an error if a passed string is not a valid hexadecimal value.
+      console.log(err.message);
+      setError(true);
+    }
   }
+
   return (
     <>
     <section className="container">
       <h3>color generator</h3>
       <form onSubmit={ handleSubmit }>
-        <input type="text" value={color} onChange={(e) => setColor( e.target.value)} placeholder='#f15025'/>
+        <input className={ error ? 'error' :null} type="text" value={color} onChange={(e) => setColor( e.target.value)} placeholder='#f15025'/>
         <button className="btn">submit</button>
       </form>
+      { error && <p>type a valid hexadecimal value</p>}
     </section>
     <section className="colors">
       <h4>list goes here</h4>
